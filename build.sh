44540ba06945ffcd870042aa1a0f179fa3b7e71c
knightmar <knightmar477@gmail.com>
2026-07-15 00:11:36 +0900

    removed unsable dl link and went with native as compiler for .s

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ export PATH=$PATH:$HOME/.cargo/bin
 make clear
 mkdir -p build/isodir/boot/grub
 
-i686-elf-as src/boot/boot.s -o ./build/boot.o
+if command -v i686-elf-as &> /dev/null; then
+    echo "i686-elf-as found, using it"
+    i686-elf-as src/boot/boot.s -o ./build/boot.o
+else
+    echo "i686-elf-as not found, switching to native assembler in 32-bit mode"
+    as --32 src/boot/boot.s -o ./build/boot.o
+fi
 
 cargo +nightly build --release --target x86-unknown-bare_metal.json || exit 1
 
